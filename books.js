9de72333c4cb476b2b6fc9dd406500b55e60932a
tushort <samquioh@gmail.com>
2023-08-23 14:00:46 -0700

    change author home town

---
+++ books.js
@@ -0,0 +1,148 @@
+const BOOKS = [{
+
+    id: 1,
+    title: "The Enchanted Garden",
+    author: "Alice Johnson",
+    hometown: "Portland",
+    married: true,
+  },
+  {
+    id: 2,
+    title: "Beyond the Horizon",
+    author: "David Miller",
+    hometown: "Los Angeles",
+    married: false,
+  },
+  {
+    id: 3,
+    title: "Whispers of the Past",
+    author: "Sarah Williams",
+    hometown: "Boston",
+    married: true,
+  },
+  {
+    id: 4,
+    title: "Shadows in the Moonlight",
+    author: "Robert Carter",
+    hometown: "San Francisco",
+    married: false,
+  },
+  {
+    id: 5,
+    title: "Echoes from Afar",
+    author: "Jennifer Lee",
+    hometown: "Chicago",
+    married: true,
+  },
+  {
+    id: 6,
+    title: "Lost in the Clouds",
+    author: "Michael Brown",
+    hometown: "Seattle",
+    married: false,
+  },
+  {
+    id: 7,
+    title: "The Forgotten Diary",
+    author: "Emily Davis",
+    hometown: "Denver",
+    married: true,
+  },
+  {
+    id: 8,
+    title: "Under the Starlit Sky",
+    author: "William Smith",
+    hometown: "New York",
+    married: false,
+  },
+  {
+    id: 9,
+    title: "Silent Whispers",
+    author: "Sophia Johnson",
+    hometown: "Austin",
+    married: true,
+  },
+  {
+    id: 10,
+    title: "The Hidden Treasure",
+    author: "Daniel Anderson",
+    hometown: "Miami",
+    married: false,
+  },
+  {
+    id: 11,
+    title: "Across the Universe",
+    author: "Olivia White",
+    hometown: "Dallas",
+    married: true,
+  },
+  {
+    id: 12,
+    title: "Beneath the Surface",
+    author: "Ethan Martinez",
+    hometown: "Houston",
+    married: false,
+  },
+  {
+    id: 13,
+    title: "The Mystic Mirror",
+    author: "Ava Taylor",
+    hometown: "Atlanta",
+    married: true,
+  },
+  {
+    id: 14,
+    title: "Voices in the Wind",
+    author: "Noah Wilson",
+    hometown: "Phoenix",
+    married: false,
+  },
+  {
+    id: 15,
+    title: "Dreams of Tomorrow",
+    author: "Emma Brown",
+    hometown: "San Diego",
+    married: true,
+  },
+  {
+    id: 16,
+    title: "The Vanishing Act",
+    author: "Liam Davis",
+    hometown: "Nashville",
+    married: false,
+  },
+  {
+    id: 17,
+    title: "Crimson Roses",
+    author: "Chloe Martinez",
+    hometown: "Cleveland",
+    married: true,
+  },
+  {
+    id: 18,
+    title: "Eternal Echoes",
+    author: "Jackson Miller",
+    hometown: "New Orleans",
+    married: false,
+  },
+  {
+    id: 19,
+    title: "Threads of Fate",
+    author: "Grace Adams",
+    hometown: "Las Vegas",
+    married: true,
+  },
+  {
+    id: 20,
+    title: "Whispers in the Wind",
+    author: "Sophia Johnson",
+    hometown: "Chicago",
+    married: false,
+  },
+];
+
+
+
+
+
+ export default BOOKS;
